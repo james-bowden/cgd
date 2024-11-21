78bd8667e6f76f1b929cb6d39104a050e32a467e
James Bowden <james.c.bowden@gmail.com>
2024-11-21 09:12:41 -0800

    batch launch scripts (remove slurm command if you use)

---
+++ launch_gauss.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for data in "data_p100_m10_n50000_nonlinear"
+do
+	for model in "linear" "linearlr" "mlplr"
+	do
+		srun -G 1 python run_gaussian.py --data-path $data --model $model &
+		srun -G 1 python run_gaussian.py --data-path $data --model $model --constraint-mode none &
+	done
+done
+wait # hopefully this lets me still cancel via ctrl-c
