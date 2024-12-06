1705a96549bc287e59b9d509996c0bbf2aeb24a4
James Bowden <james.c.bowden@gmail.com>
2024-12-06 09:41:51 -0800

    + new results sachs, still coming in

--- launch_sachs.sh
+++ launch_sachs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-srun -G 1 python run_sachsprotein.py --model linear &
-srun -G 1 python run_sachsprotein.py --model linear --constraint-mode none &
+# srun -G 1 python run_sachsprotein.py --model linear &
+# srun -G 1 python run_sachsprotein.py --model linear --constraint-mode none &
 
-# for nm in 1 2 3 4 5 10
-for nm in 5
+for nm in 1 2 3 4 10
 do
 	for model in "linearlr" "mlplr"
 	do
-		srun -G 1 python run_sachsprotein.py --model $model &
-		srun -G 1 python run_sachsprotein.py --model $model --constraint-mode none &
+		# srun -G 1 python run_sachsprotein.py --model $model --num-modules $nm &
+		# srun -G 1 python run_sachsprotein.py --model $model --num-modules $nm --constraint-mode none &
+		python run_sachsprotein.py --model $model --num-modules $nm
+		python run_sachsprotein.py --model $model --num-modules $nm --constraint-mode none
 	done
 done
 wait # hopefully this lets me still cancel via ctrl-c
